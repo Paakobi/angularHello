1d4c4a3fea833eb9948bc256e0cdcd53faea08cf
Paakobi <136978901+Paakobi@users.noreply.github.com>
2023-08-11 13:06:00 -0500

    Tour of Heroes Part 4  (Adding Services) is done

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,16 +1,41 @@
 import { Component } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent {
-   heroes: Hero [] = HEROES;
-   selectedHero?: Hero;
-   onSelect(hero: Hero): void {
+  selectedHero?: Hero;
+  heroes: Hero [] = [];
+  
+   private heroService: HeroService;
+
+    /* option wiring*/
+    // constructor( private heroService: HeroService) {}
+    /* Making this different from Tour of Heroes to make it clear but it means the same thing*/
+    constructor( heroService: HeroService) {
+      this.heroService = heroService;
+    }      
+
+    ngOnInit(): void {
+      this.getHeroes();
+    }
+
+    onSelect(hero: Hero): void {
       this.selectedHero = hero;
-}
-}
+    
+     }
+
+    getHeroes(): void {
+      this.heroService.getHeroes()
+          .subscribe(heroes => this.heroes = heroes);
+    }
+    
+
+  }
